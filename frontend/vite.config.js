4dda4c97a80c6a4f4090c201c109de63f7c296ea
Harsh <harshtayal2005@gmail.com>
2024-11-06 12:35:46 +0530

    vercel routes checking

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-      '/api': {
+      "/api": {
         target: "https://bill-craft-backend.vercel.app",
-        changeOrigin: true
-      } 
-    }   
+        changeOrigin: true,
+      },
+    },
   },
   plugins: [react()],
-})
+});
