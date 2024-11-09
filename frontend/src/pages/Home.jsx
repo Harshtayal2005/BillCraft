85d27b9defaed284e0d63965e39fbe124eca1d62
Harsh <harshtayal2005@gmail.com>
2024-11-10 02:08:38 +0530

    responsiveness added in welcome page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,12 +2,16 @@ import { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import AvatarUploader from "../components/AvatarUploader.jsx";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { ImCross } from "react-icons/im";
+
 const Home = () => {
   const navigate = useNavigate();
   const [avatar, setAvatar] = useState(null);
   const [toggle, setToggle] = useState(0);
   const [userInfo, setUserInfo] = useState([]);
   const [templates, setTemplates] = useState([1, 2, 3, 4, 5, 6, 7, 8]);
+  const [hamBurgerMenu, setHamBurgerMenu] = useState(0);
   useEffect(() => {
     const verifyUser = async () => {
       try {
@@ -50,7 +54,7 @@ const Home = () => {
     <>
       {toggle === 1 && <AvatarUploader handleClick={handleClick} />}
       <div className={`${toggle === 1 && "hidden"} min-h-screen flex`}>
-        <div className="w-[20%] bg-gradient-to-tl from-gray-900 to-lime-700 flex flex-col justify-between items-center py-7 text-white">
+        <div className="hidden w-[20%] bg-gradient-to-tl from-gray-900 to-lime-700 lg:flex flex-col justify-between items-center py-7 text-white">
           <div className="flex flex-col gap-20">
             <div className="flex gap-1 justify-center">
               <h1 className="font-great-vibes-regular font-bold text-2xl">
@@ -63,7 +67,7 @@ const Home = () => {
                 <div className="h-20 w-20 rounded-full overflow-hidden">
                   <img
                     src={avatar}
-                    alt="space"
+                    alt="avatar"
                     className="h-full w-full object-cover"
                   />
                 </div>
@@ -81,26 +85,27 @@ const Home = () => {
             </button>
           </div>
         </div>
-        <div className="w-[80%] px-8 pt-8 flex flex-col gap-14">
+        <div className="w-full lg:w-[80%] px-8 pt-8 flex flex-col gap-14">
           <div className="flex justify-between">
             <div className="flex flex-col gap-2">
-              <h1 className="text-4xl">Welcome back, {userInfo.fullname}</h1>
+              <h1 className="text-4xl font-bold">Welcome back, {userInfo.fullname}</h1>
               <h2 className="text-gray-500">
                 Take a look at all the beautiful templates available
               </h2>
             </div>
-            <div>
+            <div className="flex justify-center items-center">
               <button
                 onClick={() => navigate("/clients")}
-                className="bg-gradient-to-tl from-gray-900 to-lime-700 text-yellow-400 py-3 px-8 rounded-full"
+                className="hidden lg:block bg-gradient-to-tl from-gray-900 to-lime-700 text-yellow-400 py-3 px-8 rounded-full"
               >
                 Clients
               </button>
+              <GiHamburgerMenu className="lg:hidden text-3xl mr-3" />
             </div>
           </div>
-          <div className="flex flex-wrap justify-between gap-8">
+          <div className="flex flex-wrap justify-evenly gap-8">
             {templates.map((template) => (
-              <div key={template} onClick={() => window.open("https://bill-craft-by-harshtayal.vercel.app//template1")} className="h-[30rem] w-[20rem] bg-red-500 rounded-lg border border-gray-300 overflow-hidden">
+              <div key={template} onClick={() => window.open("https://bill-craft-by-harshtayal.vercel.app//template1")} className="h-[30rem] w-[20rem] rounded-lg border border-gray-300 overflow-hidden hover:cursor-pointer hover:scale-105 duration-500">
                 <img
                   src="template1.png"
                   alt="template1"
--- frontend/src/pages/Welcome.jsx
+++ frontend/src/pages/Welcome.jsx
@@ -5,21 +5,27 @@ const Welcome = () => {
   const navigate = useNavigate();
   return (
     <>
-      <div className="h-screen bg-black flex flex-col">
+      <div className="min-h-screen bg-black flex flex-col justify-center">
         <div className="flex text-white items-center gap-1 absolute left-4 top-4">
           <h1 className="text-3xl font-great-vibes-regular">BillCraft</h1>
           <img src="/invoice.svg" alt="" className="h-5 w-5" />
         </div>
-        <div className="h-full rounded-xl text-white flex items-center">
-          <div className="flex flex-col gap-8 w-1/2 pl-14">
+        <div className="h-full rounded-xl text-white flex flex-col lg:flex-row-reverse justify-center items-center gap-20 lg:gap-0">
+          <div className="w-full lg:w-1/2 flex justify-center items-center">
+            <div className="h-[15rem] w-[15rem] xsm:h-[20rem] xsm:w-[20rem] sm:h-[25rem] sm:w-[25rem] flex justify-center items-center rounded-full overflow-hidden bg-gradient-to-r from-slate-900 to-slate-700 shadow-[0px_0px_20px_10px_#81e6d9]">
+              <p className="text-7xl xsm:text-8xl sm:text-9xl font-dancing-script">Hello</p>
+            </div>
+          </div>
+          <div className="flex flex-col items-center lg:items-start gap-8 w-full lg:w-1/2 lg:pl-14">
             <div className="flex items-center gap-2">
-              <h1 className="text-7xl">
+              <h1 className="text-3xl xsm:text-5xl md:text-7xl">
                 Welcome to{" "}
                 <span className="font-great-vibes-regular">BillCraft</span>
               </h1>
+              {/* <img src="invoice.svg" alt="logo" className="lg:hidden h-10 w-10 md:h-14 md:w-14" /> */}
             </div>
             <div className="w-2/3 text-gray-400">
-              <p>
+              <p className="text-center lg:text-start text-xs xsm:text-sm sm:text-base md:text-lg">
                 Effortlessly manage clients and create custom invoices using our
                 beautiful, customizable templates.
               </p>
@@ -27,15 +33,10 @@ const Welcome = () => {
             <div>
               <button
                 onClick={() => navigate("/register")}
-                className="bg-teal-600 py-3 px-8 rounded-3xl font-bold text-2xl"
+                className="bg-teal-600 py-3 px-6 sm:py-4 sm:px-8 rounded-2xl font-bold text-xl sm:text-2xl"
               >
                 Get Started
               </button>
-            </div>
-          </div>
-          <div className="w-1/2 flex justify-center items-center">
-            <div className="h-[25rem] w-[25rem] flex justify-center items-center rounded-full overflow-hidden bg-gradient-to-r from-slate-900 to-slate-700 shadow-[0px_0px_20px_10px_#81e6d9]">
-              <p className="text-9xl font-dancing-script">Hello</p>
             </div>
           </div>
         </div>
